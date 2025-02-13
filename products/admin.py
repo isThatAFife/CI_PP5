3c0078b7c76237b6a4dbaf9c3a5219a4eb985456
D <dylankfilm@gmail.com>
2025-02-13 14:09:44 +0900

    fix: changed featured_image to image in products model

--- products/admin.py
+++ products/admin.py
@@ -2,5 +2,23 @@ from django.contrib import admin
 from .models import Category, Product
 
 # Register your models here.
-admin.site.register(Category)
-admin.site.register(Product)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'price',
+        'image',
+    )
+
+    ordering = ('category',)
+    
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+    
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
