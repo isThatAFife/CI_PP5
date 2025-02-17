4d8a193d159cf82797fe7b9a1f4a211003b18d5b
D <dylankfilm@gmail.com>
2025-02-17 21:03:49 +0900

    feat: admin add product functionality added

--- PP5/settings.py
+++ PP5/settings.py
@@ -180,6 +180,8 @@ STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv("STRIPE_WH_SECRET", "")
 
+DEFAULT_FROM_EMAIL = 'mikeysculpts@example.com'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
