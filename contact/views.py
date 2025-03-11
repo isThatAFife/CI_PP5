607d9bb7dcf06cc7f0d92607cf5ee38618b7ab1e
D <dylankfilm@gmail.com>
2025-03-11 21:14:09 +0900

    fix: fixed contact view to include contact/contact.html

--- contact/views.py
+++ contact/views.py
@@ -5,7 +5,7 @@ from django.contrib import messages
 
 class ContactFormView(FormView):
     form_class = ContactForm
-    template_name = 'contact.html'
+    template_name = 'contact/contact.html'
     success_url = reverse_lazy('contact_success')
 
     def form_valid(self, form):
