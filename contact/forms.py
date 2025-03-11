06b60225cfcd352b4407d4afd328d69478845f71
D <dylankfilm@gmail.com>
2025-03-11 22:56:48 +0900

    style: resized message field in contact form

--- contact/forms.py
+++ contact/forms.py
@@ -1,22 +1,45 @@
+# contact/forms.py
 from django import forms
 from .models import ContactMessage
 from django.core.validators import MinLengthValidator
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Field, Submit
 
 class ContactForm(forms.ModelForm):
     name = forms.CharField(
-        validators=[MinLengthValidator(2, "Name must be at least 2 characters")]
+        validators=[MinLengthValidator(2, "Name must be at least 2 characters")],
+        widget=forms.TextInput(attrs={'class': 'form-control'})
     )
     message = forms.CharField(
-        validators=[MinLengthValidator(10, "Message must be at least 10 characters")]
+        validators=[MinLengthValidator(10, "Message must be at least 10 characters")],
+        widget=forms.Textarea(attrs={
+            'rows': 8,
+            'class': 'tall-message-field form-control',
+            'style': 'min-height:150px;'
+        })
     )
-    
+
     class Meta:
         model = ContactMessage
         fields = ['name', 'email', 'subject', 'message']
         widgets = {
-            'message': forms.Textarea(attrs={
-                'rows': 8, 
-                'class': 'large-message' 
-            }),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'subject': forms.TextInput(attrs={'class': 'form-control'}),
         }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
         
+        # Layout configuration
+        self.helper.layout = Layout(
+            Field('name', css_class='col-md-6 mb-3'),
+            Field('email', css_class='col-md-6 mb-3'),
+            Field('subject', css_class='col-md-6 mb-3'),
+            Field('message', css_class='tall-message-field mb-3'),
+            Submit('submit', 'Send', css_class='btn-primary')
+        )
+        
+        # Force left-aligned labels
+        self.helper.label_class = 'text-left col-form-label'
