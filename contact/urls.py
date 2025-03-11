9bd6ce195eef210dd57d41511df803ba7d7b28ad
D <dylankfilm@gmail.com>
2025-03-11 21:05:10 +0900

    fix: fixed contact urls.py

--- contact/urls.py
+++ contact/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import ContactFormView
 
 urlpatterns = [
-    path('contact/', ContactFormView.as_view(), name='contact'),
+    path('', ContactFormView.as_view(), name='contact'),
 ]
