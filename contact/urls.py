ce10b7875bb5e68a577e8b346720dd2aaa5112e4
D <dylankfilm@gmail.com>
2025-03-14 16:10:16 +0900

    feature: added email settings to receive contact form

--- contact/urls.py
+++ contact/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ContactFormView
+from .views import ContactFormView, ContactSuccessView
 
 urlpatterns = [
     path('', ContactFormView.as_view(), name='contact'),
+    path('success/', ContactSuccessView.as_view(), name='contact_success'),
 ]
--- contact/views.py
+++ contact/views.py
@@ -1,7 +1,9 @@
-from django.views.generic import FormView
+from django.views.generic import FormView, TemplateView
 from django.urls import reverse_lazy
 from .forms import ContactForm
 from django.contrib import messages
+from django.core.mail import send_mail
+from django.conf import settings
 
 class ContactFormView(FormView):
     form_class = ContactForm
@@ -10,5 +12,16 @@ class ContactFormView(FormView):
 
     def form_valid(self, form):
         form.save()
+        send_mail(
+            subject=f"New message from {form.cleaned_data['name']}",
+            message=form.cleaned_data['message'],
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            recipient_list=[settings.NOTIFY_EMAIL],
+            fail_silently=False,
+        )
         messages.success(self.request, "Message received successfully!")
         return super().form_valid(form)
+    
+    
+class ContactSuccessView(TemplateView):
+    template_name = 'contact_success.html'
