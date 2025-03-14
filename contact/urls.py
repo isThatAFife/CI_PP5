365c3908f538c0a13ace7affa9b8d3927ac85430
D <dylankfilm@gmail.com>
2025-03-14 16:49:18 +0900

    fix: fixed urls.py in contact

--- contact/urls.py
+++ contact/urls.py
@@ -3,5 +3,5 @@ from .views import ContactFormView, ContactSuccessView
 
 urlpatterns = [
     path('', ContactFormView.as_view(), name='contact'),
-    path('success/', ContactSuccessView.as_view(), name='contact_success'),
+    path('', ContactSuccessView.as_view(), name='contact_success'),
 ]
